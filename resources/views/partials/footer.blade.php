a1b716982b2211082a5786c1d96f65a75d72942b
Pawel1k <151437674+Pawel1k@users.noreply.github.com>
2026-04-30 19:54:54 +0200

    Add files via upload

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+  <div class="footer-container">
+    
+    <div class="footer-col">
+      <h3>EQUIPRENT PRO</h3>
+      <p>
+        zarządzanie wysokowydajnym sprzętem sportowym, 
+        standaryzowane rozwiązania wynajmu dla 
+        profesjonalnych projektów i zawodów.
+      </p>
+    </div>
+
+    <div class="footer-col">
+      <h4>FLOTA SPRZĘTU</h4>
+      <ul>
+        <li><a href="#">Rowery MTB</a></li>
+        <li><a href="#">Rowery szosowe</a></li>
+        <li><a href="#">E-bikes</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-col">
+      <h4>FIRMA</h4>
+      <ul>
+        <li><a href="#">O nas</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+  <div class="footer-bottom">
+    <a href="#">Polityka bezpieczeństwa</a>
+    <a href="#">Regulamin</a>
+  </div>
+</footer>

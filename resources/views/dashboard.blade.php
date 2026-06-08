085cdf709252d4e7f2dd84d409f7bbb079795ba1
Pawel1k <252335@edu.p.lodz.pl>
2026-06-08 22:44:32 +0200

    nic nowego

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@
                     {{-- Niebieski - Stan sprzętu --}}
                     <div class="db-stat dark">
                         <div class="db-stat-label">
-                            Stan sprzętu
+                            Zarobki w twoim tygodniu
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"/><polyline points="17 6 23 6 23 12"/></svg>
                         </div>
                         <div class="db-dark-title">
